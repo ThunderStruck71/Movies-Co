d206ad31d11953e63abd02d25641b27f60517036
ThunderStruck71 <iovodovi@yandex.ru>
2024-05-14 02:25:50 +0300

    Add front start sources

---
+++ sources/front/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <h1>
+    Movies&Co.
+  </h1>
+
+  <main>
+    Welcome to my amazing project 😊
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
