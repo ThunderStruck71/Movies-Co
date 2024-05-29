b92afe14619dc55b330fb1ff72b920a1269087c5
ThunderStruck71 <iovodovi@yandex.ru>
2024-05-30 02:26:47 +0300

    Initialize all services in main

--- sources/back/cmd/main.go
+++ sources/back/cmd/main.go
@@ -1,17 +1,79 @@
 package main
 
 import (
-	"back"
-	"fmt"
+	"back/internal/app/handler"
+	"back/internal/app/service"
+	"back/internal/domain/repository"
+	"context"
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	port := "8080"
-	srv := new(back.Server)
-
-	fmt.Printf("Server was running on port %s...\n", port)
-	if err := srv.Run(port); err != nil {
-		log.Fatalf("Something wrong while running server: %s", err.Error())
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
 	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	log.Println("Successfully connected to database")
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	e := handlers.InitRoutes()
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	//Start server
+	go func() {
+		if err := e.Start(":1323"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	log.Println("Movies-Co app was started")
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	log.Println("Movies-Co app was finished")
+
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	if err := db.Close(); err != nil {
+		log.Fatalf("error occured on db connection close: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
